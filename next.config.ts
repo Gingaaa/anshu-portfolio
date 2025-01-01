a57b79cc6b0ff2cc66aea474a5641096b1324e54
Anshu Kumar <93460183+Gingaaa@users.noreply.github.com>
2025-01-01 14:36:39 +0530

    headers modified

--- next.config.ts
+++ next.config.ts
@@ -7,20 +7,13 @@ const nextConfig: NextConfig = {
         source: "/(.*)",
         headers: [
           {
-            key: "Content-Security-Policy",
-            value: "default-src 'self'; script-src 'self'",
-          },
-          {
             key: "X-DNS-Prefetch-Control",
             value: "on",
           },
-          { key: "X-Content-Type-Options", value: "nosniff" },
-          { key: "X-Frame-Options", value: "DENY" },
           {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains; preload",
           },
-          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
         ],
       },
     ];
